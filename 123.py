a43f54d8d4d205b4b125720ab8ea0ef0597cd62d
pb12ba02 <pb12ba02@gmail.com>
2021-05-07 23:48:02 +0200

    try

--- 123.py
+++ 123.py
@@ -1,12 +1,16 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
 #temperlature
+for i in range(3):
 val = input("degree:")
+if val[-1] in ['c', 'C']:
+    f = 1.8 * float(val[0:-1]) + 32
+    print("trun to: %.2fF"%f)
+elif val[-1] in ['F', 'f']:
+    c = (float(val[0:-1]) - 32) / 1.8
+    print("trun to: %.2fc"%c)
+else:
+    print("wrong")
+
+#PLACE
+val = input("location")
+
+
